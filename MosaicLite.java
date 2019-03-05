9d1bb491858161f8796a1384b783b4b9bfe783da
matthewrgoff <matthewrgoff@lewisu.edu>
2019-03-04 22:40:29 -0600

    Implement v0-03.

--- MosaicLite.java
+++ MosaicLite.java
@@ -7,9 +7,65 @@ import javax.swing.JButton;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
-//import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.Random;
+
+class XAndOTile extends JPanel {
+    private int red, green, blue;
+    private String letter;
+
+    XAndOTile() {
+        super();
+        SetRandomValue();
+    }
+
+    final public void SetRandomValue() {
+        red = GetNumberBetween(0, 255);
+        green = GetNumberBetween(0, 255);
+        blue = GetNumberBetween(0, 255);
+
+        letter = "X";
+        if (GetNumberBetween(0,1) == 1) {
+            letter = "O";
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        g.setColor(new Color(red,green,blue));
+        g.fillRect(10,10,panelWidth-20,panelHeight-20);
+
+        g.setColor(new Color(GetContrastingColor(red),GetContrastingColor(green),GetContrastingColor(blue)));
+            
+        final int fontSize=100;
+        g.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
+        int stringX = (panelWidth/2)-30;
+        int stringY = (panelHeight/2)+30;
+        g.drawString(letter,stringX,stringY);
+    }
+
+    private static int GetContrastingColor(int colorIn) {
+        return ((colorIn+128)%256);
+    }
+
+    private static int GetNumberBetween(int min, int max) {
+        Random myRandom = new Random();
+        return min + myRandom.nextInt(max-min+1);
+    }
+}
 
 class MosaicLiteFrame extends JFrame {
+    //private ArrayList<XAndOTile>tileList;
+
     public MosaicLiteFrame() {
         setBounds(10,10,800,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,7 +83,12 @@ class MosaicLiteFrame extends JFrame {
         contentPane.add(xAndOGridPanel, BorderLayout.CENTER);
         xAndOGridPanel.setLayout(new GridLayout(3,3));
 
-        
+        //tileList = new ArrayList<XAndOTile>();
+        for(int i=1; i<10; i++) {
+            XAndOTile tile = new XAndOTile();
+            //tileList.add(tile);
+            xAndOGridPanel.add(tile);
+        }
     }
 }
 
