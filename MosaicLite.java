eb898383f10283970ec09b7917daaa3abec9517d
matthewrgoff <matthewrgoff@lewisu.edu>
2019-03-04 22:01:31 -0600

    Implement v0-02.

--- MosaicLite.java
+++ MosaicLite.java
@@ -2,11 +2,32 @@
 // Mosaic Lite
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+//import java.awt.FlowLayout;
 
 class MosaicLiteFrame extends JFrame {
     public MosaicLiteFrame() {
-        setBounds(10,10,1200,600);
+        setBounds(10,10,800,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        JPanel buttonPanel = new JPanel();
+        contentPane.add(buttonPanel, BorderLayout.SOUTH);
+
+        JButton randomize = new JButton("Randomize");
+        buttonPanel.add(randomize);
+
+        JPanel xAndOGridPanel = new JPanel();
+        contentPane.add(xAndOGridPanel, BorderLayout.CENTER);
+        xAndOGridPanel.setLayout(new GridLayout(3,3));
+
+        
     }
 }
 
